00c1e1827259e3fe669a2aa61825a60e9a072b9e
sinapirani <realsinapirani@gmail.com>
2022-05-20 17:35:30 +0430

    button added ✨

--- components/home/buttons.js
+++ components/home/buttons.js
@@ -1,3 +1,14 @@
 
+const HomeButton = () => {
 
+    return (
+      <div className=" flex flex-col gap-y-2 justify-center items-center">
+        <button className=" w-[22rem] h-28 bg-white text-[#182729] text-5xl font-extrabold rounded-2xl">
+          Play Now
+        </button>
+        <p>Login to your account</p>
+      </div>
+    );
+}
 
+export default HomeButton
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 
 import Image from 'next/image'
+import HomeButton from '../components/home/buttons';
 import HomeHead from '../components/home/head'
 import HomeText from '../components/home/text'
 // import styles from '../styles/Home.module.css'
@@ -15,8 +16,9 @@ export default function Home() {
       <div className="absolute flex justify-center items-center w-full h-screen box-border bg-[rgba(0,0,0,80%)] overflow-hidden pointer-events-none z-40">
         <div className=" absolute h-[150vh] w-[150vh] bg-[#006f7e] opacity-25 blur-[188px] rounded-full z-0 "></div>
 
-        <div className="flex justify-between items-center text-white z-10">
+        <div className=" w-full flex justify-evenly items-center text-white z-10">
           <HomeText />
+          <HomeButton/>
         </div>
       </div>
     </div>

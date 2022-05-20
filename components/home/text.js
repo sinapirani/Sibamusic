1f4c8596de4bddc561844945e11ab6da6fc3fdf4
sinapirani <realsinapirani@gmail.com>
2022-05-20 17:29:00 +0430

    home page and text added🥳

---
+++ components/home/text.js
@@ -0,0 +1,11 @@
+
+
+const HomeText = () => {
+
+    return(
+        <>
+        <h1 className=" text-8xl font-black">Play Music <br/> But Beautiful</h1>
+        </>
+    )
+}
+export default HomeText
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
-import Head from 'next/head'
+/* eslint-disable @next/next/no-img-element */
+
 import Image from 'next/image'
 import HomeHead from '../components/home/head'
-import styles from '../styles/Home.module.css'
+import HomeText from '../components/home/text'
+// import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}> 
+    <div className=" relative w-full h-screen overflow-hidden m-0 p-0 flex justify-center items-center bg-black ">
+      <HomeHead />
 
-      <HomeHead/>
-      
+      <div className="absolute w-full h-screen bg-red-600 z-20 overflow-hidden bg-[url(/home/bg.jpg)] bg-cover bg-center blur-xl scale-150 "></div>
 
+      <div className="absolute flex justify-center items-center w-full h-screen box-border bg-[rgba(0,0,0,80%)] overflow-hidden pointer-events-none z-40">
+        <div className=" absolute h-[150vh] w-[150vh] bg-[#006f7e] opacity-25 blur-[188px] rounded-full z-0 "></div>
+
+        <div className="flex justify-between items-center text-white z-10">
+          <HomeText />
+        </div>
+      </div>
     </div>
-  )
+  );
 }

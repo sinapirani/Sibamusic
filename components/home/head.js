0b3fc0730452edf2c89a6fca35c5fdfc74701064
sinapirani <realsinapirani@gmail.com>
2022-05-20 12:05:33 +0430

    tailwind configed🥳

---
+++ components/home/head.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+const HomeHead = () => {
+    return(
+      <Head>
+        <title>Siba Music Player</title>
+        <meta name="description" content="play musics but beautiful | siba music" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    )
+}
+
+export default HomeHead
